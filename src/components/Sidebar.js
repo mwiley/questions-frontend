8e7a603c0f1fc604be1ab2d8ce78524c094b17e1
Mason <masonwiley92@gmail.com>
2023-01-06 12:21:36 -0800

    fix indent level

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,49 +7,49 @@ import avatar from '../images/avatar.png';
 import './Sidebar.css';
 
 const Sidebar = () => (
-    <div className="Sidebar">
-      <Card className="Sidebar-card">
-        <h2>About</h2>
-        <p>
-          <img src={usersIcon} alt="" className="card-icon" width="25"></img>
-          246 Experts
-        </p>
-        <p>
-          <img src={questionsIcon} alt="" className="card-icon" width="25"></img>
-          100k Questions and Answers
-        </p>
-        <p>
-          <img src={upvotesIcon} alt="" className="card-icon" width="25"></img>
-          50k Upvotes
-        </p>
-        <p>
-          <img src={tokensIcon} alt="" className="card-icon" width="25"></img>
-          145 Tokens Awarded
-        </p>
-      </Card>
+  <div className="Sidebar">
+    <Card className="Sidebar-card">
+      <h2>About</h2>
+      <p>
+        <img src={usersIcon} alt="" className="card-icon" width="25"></img>
+        246 Experts
+      </p>
+      <p>
+        <img src={questionsIcon} alt="" className="card-icon" width="25"></img>
+        100k Questions and Answers
+      </p>
+      <p>
+        <img src={upvotesIcon} alt="" className="card-icon" width="25"></img>
+        50k Upvotes
+      </p>
+      <p>
+        <img src={tokensIcon} alt="" className="card-icon" width="25"></img>
+        145 Tokens Awarded
+      </p>
+    </Card>
 
-      <Card>
-        <h2>Top Experts</h2>
+    <Card>
+      <h2>Top Experts</h2>
 
-        {
-          ['Psuedo Near Expert', 'Near Expert', 'Bitcoin Maxi', 'Ethereum Maxi'].map((name, index) => {
-            return (
-              <div className='user' key={index}>
-                <img className='user-avatar' alt={`${name}'s avatar`} src={avatar} width="50" height="50"/>
-                <div className='user-info'>
-                  <span className="user-name">{name}</span>
-                  <span className="user-stats"> 
-                    47
-                    <img src={upvotesIcon} alt="upvotes" width={15}/>
-                    collected
-                  </span>
-                </div>
+      {
+        ['Psuedo Near Expert', 'Near Expert', 'Bitcoin Maxi', 'Ethereum Maxi'].map((name, index) => {
+          return (
+            <div className='user' key={index}>
+              <img className='user-avatar' alt={`${name}'s avatar`} src={avatar} width="50" height="50"/>
+              <div className='user-info'>
+                <span className="user-name">{name}</span>
+                <span className="user-stats"> 
+                  47
+                  <img src={upvotesIcon} alt="upvotes" width={15}/>
+                  collected
+                </span>
               </div>
-            )
-          })
-        }
-      </Card>
-    </div>
+            </div>
+          )
+        })
+      }
+    </Card>
+  </div>
 )
 
 export default Sidebar;
